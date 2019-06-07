feec2d5039a8fbf8aa03fcde25c96987584c0f87
Your Name <you@example.com>
2019-06-07 20:23:55 +0800

    cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 pwd
+openssl aes-256-cbc -k "$super_secret_password" -in super_secret.txt.enc -out super_secret.txt -d
 ls
 ssh -o StrictHostKeyChecking=no -i super_secret.txt root@47.92.105.16 /workspace/goblog/cd.sh
