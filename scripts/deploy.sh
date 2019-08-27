6c6dfb7a29edad9d102dbd0cb9755c6afbeb1a80
Your Name <you@example.com>
2019-08-27 18:20:40 +0200

    cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push mydocker3013/goblog:$TRAVIS_COMMIT
 openssl aes-256-cbc -k "$super_secret_password" -in super_secret.txt.enc -out super_secret.txt -d
 chmod 400 super_secret.txt
 ssh -o StrictHostKeyChecking=no -i super_secret.txt root@47.92.105.16 /workspace/goblog/cd.sh
