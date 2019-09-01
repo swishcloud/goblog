8ac9cf6f22f98612ac5d2feb0d8012666996980b
Your Name <you@example.com>
2019-09-01 11:38:41 +0200

    cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+#define environment variables
+server_deploy_file_path="/workspace/docker-stack/goblog/cd.sh"
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push mydocker3013/goblog:$TRAVIS_COMMIT
+if [ $? -neq 0 ]; then
+    return 1
+fi
+docker push $IMAGE_TAG
 openssl aes-256-cbc -k "$super_secret_password" -in super_secret.txt.enc -out super_secret.txt -d
 chmod 400 super_secret.txt
-ssh -o StrictHostKeyChecking=no -i super_secret.txt root@47.92.105.16 /workspace/goblog/cd.sh $TRAVIS_COMMIT
+ssh -o StrictHostKeyChecking=no -i super_secret.txt root@47.92.105.16 $server_deploy_file_path $TRAVIS_COMMIT
+if [ $? -ne 0 ]; then
+    exit 1
+else
+    echo "-----------------------------"
+    echo "deploy completed successfully"
+    echo "-----------------------------"
+fi
