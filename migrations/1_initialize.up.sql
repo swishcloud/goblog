120a528e06ee05274400ee52c3d512e0ab9c255e
allen <allen@DESKTOP-BM3TNO6>
2020-01-09 18:18:14 +0800

    migration

--- migrations/1_initialize.up.sql
+++ migrations/1_initialize.up.sql
@@ -1,137 +1,81 @@
--- MySQL dump 10.13  Distrib 8.0.13, for Linux (x86_64)
---
--- Host: localhost    Database: goblog
--- ------------------------------------------------------
--- Server version       8.0.13
+-- Table: public."user"
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- DROP TABLE public."user";
 
---
--- Table structure for table `article`
---
+CREATE TABLE public."user"
+(
+    id serial NOT NULL,
+    user_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    level2pwd character varying(200) COLLATE pg_catalog."default",
+    insert_time timestamp without time zone NOT NULL,
+    update_time timestamp without time zone,
+    is_banned boolean NOT NULL,
+    op_issuer character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    op_userid character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    avatar character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id),
+    CONSTRAINT email_uniquekey UNIQUE (email)
+,
+    CONSTRAINT user_name_uniquekey UNIQUE (user_name)
 
-DROP TABLE IF EXISTS `article`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `categoryId` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
-  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `content` varchar(5000) NOT NULL,
-  `insertTime` datetime NOT NULL,
-  `updateTime` datetime DEFAULT NULL,
-  `isDeleted` bit(1) NOT NULL,
-  `isBanned` bit(1) NOT NULL,
-  `html` varchar(10000) NOT NULL,
-  `summary` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `cover` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_userId_idx` (`userId`),
-  KEY `fk_blog_categoryId_idx` (`categoryId`),
-  CONSTRAINT `fk_blog_categoryId` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_blog_userId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=502 DEFAULT CHARSET=utf8 COMMENT='                                                                                           ';
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Table structure for table `articleBackup`
---
+-- Table: public.category
 
-DROP TABLE IF EXISTS `articleBackup`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `articleBackup` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(10000) NOT NULL,
-  `articleId` int(11) NOT NULL,
-  `insertTime` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_articleBackup_articleId_idx` (`articleId`),
-  CONSTRAINT `fk_articleBackup_articleId` FOREIGN KEY (`articleId`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=250 DEFAULT CHARSET=utf8 COMMENT='                                                       ';
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- DROP TABLE public.category;
 
---
--- Table structure for table `category`
---
+CREATE TABLE public.category
+(
+    id serial NOT NULL,
+    name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    insert_time timestamp without time zone NOT NULL,
+    update_time timestamp without time zone,
+    is_deleted boolean NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT category_pkey PRIMARY KEY (id),
+    CONSTRAINT user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `insertTime` date NOT NULL,
-  `updateTime` date DEFAULT NULL,
-  `isDeleted` bit(1) NOT NULL,
-  `userId` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `userId_idx` (`userId`),
-  CONSTRAINT `fk_category_userId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Table: public.article
 
---
--- Table structure for table `user`
---
+-- DROP TABLE public.article;
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `level2pwd` varchar(200) DEFAULT NULL,
-  `insertTime` datetime NOT NULL,
-  `updateTime` datetime DEFAULT NULL,
-  `isDeleted` bit(1) NOT NULL,
-  `isBanned` bit(1) NOT NULL,
-  `accessFailedCount` int(11) NOT NULL,
-  `lockoutEnd` datetime DEFAULT NULL,
-  `securityStamp` varchar(45) DEFAULT NULL,
-  `emailConfirmed` int(1) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userName_UNIQUE` (`userName`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `wsmessage`
---
-
-DROP TABLE IF EXISTS `wsmessage`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `wsmessage` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `insertTime` datetime NOT NULL,
-  `msg` varchar(500) NOT NULL,
-  `isDeleted` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=253 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-08-05 15:02:51
+CREATE TABLE public.article
+(
+    id serial NOT NULL,
+    category_id integer NOT NULL,
+    content text COLLATE pg_catalog."default" NOT NULL,
+    html text COLLATE pg_catalog."default" NOT NULL,
+    insert_time timestamp without time zone NOT NULL,
+    is_banned boolean NOT NULL,
+    is_deleted boolean NOT NULL,
+    title text COLLATE pg_catalog."default" NOT NULL,
+    type integer NOT NULL,
+    update_time timestamp without time zone,
+    user_id integer NOT NULL,
+    cover character varying(100) COLLATE pg_catalog."default",
+    summary text COLLATE pg_catalog."default" NOT NULL,
+    backup_article_id integer,
+    remark character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT article_pkey PRIMARY KEY (id),
+    CONSTRAINT backup_article_id_fkey FOREIGN KEY (backup_article_id)
+        REFERENCES public.article (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT category_id_fkey FOREIGN KEY (category_id)
+        REFERENCES public.category (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
