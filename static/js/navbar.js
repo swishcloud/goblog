6d1340da7df825f953e3d884c0165f6a377c72e9
swishcloud <flwwd@outlook.com>
2020-07-11 12:25:19 +0800

    styles

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -12,3 +12,14 @@ $(".menu-item").mouseleave(function () {
 $(".menu-item").mouseenter(function () {
     $(this).find(".sub-menu").css("display","block")
 })
+let last_known_scroll_position = 0;
+window.addEventListener('scroll', function(e) {
+if( window.scrollY>last_known_scroll_position){
+    $(".nav").css("top","-50px")
+    $(".nav").css("opacity","0")
+}else{
+    $(".nav").css("top","0px")
+    $(".nav").css("opacity","1")
+}
+last_known_scroll_position = window.scrollY;    
+});
