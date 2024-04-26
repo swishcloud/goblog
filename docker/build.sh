ba629e8fa90fb61a757ba842d3b2ef753e3720b1
Your Name <you@example.com>
2024-04-26 21:39:43 +0800

    Revert "update docker/build.sh"
    This reverts commit 3d1a0fe432d8c83c27ac617cc307dca3fe726684.

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,6 @@
 #/bin/sh
-if [ -d ./.dist ]
-then
-rm ./.dist -r
-mkdir ./.dist
-fi
-
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./.dist/app
 cp templates  ./.dist -r
 cp static  ./.dist -r
 cp migrations  ./.dist -r
-#sudo docker build --tag $IMAGE_TAG -f docker/dockerfile ./.dist
+sudo docker build --tag $IMAGE_TAG -f docker/dockerfile ./.dist
